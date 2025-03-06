0c413df68e576640c00795edb1190c7e7ed1c150
鲜青松 <1984564067@qq.com>
2025-03-06 18:43:39 +0800

    第一次

--- src/main/java/com/xian/Main.java
+++ src/main/java/com/xian/Main.java
@@ -3,6 +3,7 @@ package com.xian;
 //TIP 要<b>运行</b>代码，请按 <shortcut actionId="Run"/> 或
 // 点击装订区域中的 <icon src="AllIcons.Actions.Execute"/> 图标。
 public class Main {
+    //你好
     public static void main(String[] args) {
         //TIP 当文本光标位于高亮显示的文本处时按 <shortcut actionId="ShowIntentionActions"/>
         // 查看 IntelliJ IDEA 建议如何修正。
